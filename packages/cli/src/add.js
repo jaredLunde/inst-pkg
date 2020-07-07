84801e77ffea524422a06c0d53f2826132cb4714
Jared Lunde <jared.lunde@gmail.com>
2020-07-06 20:48:36 -0600

    fix(cli): fix initial commit message

--- packages/cli/src/add.js
+++ packages/cli/src/add.js
@@ -285,7 +285,7 @@ export default async function add({name, template, cwd, ...args}) {
       cd ${variables.PKG_DIR}
       git init
       git add .
-      git commit -m "chore: create package from template: \\"${templateName}\\"" --no-verify
+      git commit -m "chore: create package from ${templateName}" --no-verify
     `)
   }
   // installs the template package dependencies
